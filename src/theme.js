416aa3f048720615ccd021ebfd32131b7ef9eafa
Vitoria Cristina Pacheco de Jesus <vitoria.jesus@printi.local>
2021-08-19 20:59:44 -0300

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#000014',
+    text: '#0000B3',
+    sucess: '#135E01',
+  },
+  fonts: {
+    regular: 'Roboto, sans-serif',
+  },
+};
+
+export default theme;
