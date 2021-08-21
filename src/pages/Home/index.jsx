0c2281e506afd82d22fbdd7cf8aecd46c9526ff5
Vitoria Pacheco <vitoriac.pac@gmail.com>
2021-08-21 11:19:17 -0300

    Add detalhes ao modal

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,17 +8,19 @@ import logo from '../../assets/logo.svg'
 import restaurante from '../../assets/restaurante-fake.png'
 import { Card, RestaurantCard, Modal, Map } from '../../components'
 
-import { Container, Search, Logo, Wrapper, CarouselTitle } from './styles'
+import { Container, Search, Logo, Wrapper, CarouselTitle, ModalTitle, ModalContent } from './styles'
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
   const [query, setQuery] = useState(null);
+  const [placeId, setPlaceId] = useState(null)
   const [modalOpened, setModalOpened] = useState(false);
-  const { restaurants } = useSelector((state) => state.restaurants);
+  const { restaurants, restaurantSelected } = useSelector((state) => state.restaurants);
 
   const settings = {
     dots: false,
     infinite: true,
+    autoplay: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
@@ -29,6 +31,11 @@ const Home = () => {
     if(e.key === 'Enter') {
       setQuery(inputValue);
     }
+  }
+
+  function handleOpenModal(placeId) {
+    setPlaceId(placeId);
+    setModalOpened(true);
   }
 
   return (
@@ -57,12 +64,20 @@ const Home = () => {
         </Search>
 
         {restaurants.map(
-          (restaurant) => (<RestaurantCard restaurant={restaurant}/>
+          (restaurant) => (<RestaurantCard
+            onClick={() => handleOpenModal(restaurant.place_id)}
+            restaurant={restaurant}/>
         ))}
 
       </Container>
-      <Map query={query} />
-      <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}/>
+      <Map query={query} placeId={placeId} />
+      <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}>
+        <ModalTitle>{restaurantSelected?.name}</ModalTitle>
+        <ModalContent>{restaurantSelected?.formatted_phone_number}</ModalContent>
+        <ModalContent>{restaurantSelected?.formatted_address}</ModalContent>
+        <ModalContent>{restaurantSelected?.opening_hours?.open_now ? 'Aberto agora :)'
+        : 'Infelizmente estamos fechados :('}</ModalContent>
+      </ Modal>
     </Wrapper>  
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -33,7 +33,7 @@ export const Map = styled.div`
 
 export const Carousel = styled(Slider)`
   .slick-slide {
-    margin-right: 1rem;
+    margin-right: 2rem;
   }
 `;
 
@@ -44,4 +44,24 @@ export const CarouselTitle = styled.h1`
   font-weight: bold;
   line-height: 2rem;
   margin: 1rem 0;
+`;
+
+export const ModalTitle = styled.p`
+  margin-bottom: 0.75rem;
+  letter-spacing: 0.11px;
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  text-transform: none;
+  line-height: 29px;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+export const ModalContent = styled.p`
+  margin-bottom: 0.75rem;
+  font-family: ${(props) => props.theme.fonts.regular};
+  font-weight: normal;
+  color: ${(props) => props.theme.colors.text};
+  line-height: 19px;
+  font-size: 1rem;
 `;
